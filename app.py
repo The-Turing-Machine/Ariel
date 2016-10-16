9e17578fc91db40d4dd32f779f65869c6d7f012d
arsh23 <programmer.arsh@gmail.com>
2016-10-16 08:52:16 +0530

    fixed shit

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask import render_template,request,flash,url_for,redirect,session
+from flask import render_template, request, flash, url_for, redirect, session
 import os
 from flask_cors import CORS, cross_origin
 import json
 from pprint import pprint
 import random
 import nltk
-import speech_to_text
-import text_to_speech
+# import speech_to_text
+# import text_to_speech
 import requests
 from nltk.stem import SnowballStemmer
 from nltk.corpus import stopwords
@@ -16,115 +16,122 @@ snowball_stemmer = SnowballStemmer("english")
 app = Flask(__name__)
 CORS(app)
 
-api_keys = ['d687a4bbd8b4a787b2f8c841db313763','806706827f427b6a77ff242b936ae973','9667ebccf1c18f24c76263a34352ca07','ef933fa89506062e98ba214165fdad5f','aba132076d484dcf8d0a69a299561c8e']
+api_keys = ['d687a4bbd8b4a787b2f8c841db313763', '806706827f427b6a77ff242b936ae973',
+            '9667ebccf1c18f24c76263a34352ca07', 'ef933fa89506062e98ba214165fdad5f', 'aba132076d484dcf8d0a69a299561c8e']
 
 app.secret_key = "secret"
-@app.route('/',methods=['GET','POST'])
-def index():
-  	if request.method == 'POST':
-		# data = json.loads(request.data)
-		data = request.json
-		print data
-		print data.decode('utf-8')
-		return str(data)
 
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    print 'root'
+    if request.method == 'POST':
+        # data = json.loads(request.data)
+        data = request.json
+        print data
+        # print data.decode('utf-8')
+        return str(data)
+    else:
+        return 'home'
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
 def read_json(words):
-	with open('data.json') as data_file:
-		data = json.load(data_file)
-		# pprint(data)
+    with open('data.json') as data_file:
+        data = json.load(data_file)
+        # pprint(data)
 
-		keys =  data.keys()
-		print keys
-		if len(words)==0:
-			print "Empty"
+        keys = data.keys()
+        print keys
+        if len(words) == 0:
+            print "Empty"
 
-		final_data = []
-		for j in words:
-			val = random.randint(0,4)
-			print api_keys[val]
-			new_url = "http://words.bighugelabs.com/api/2/"+str(api_keys[val])+"/"+j[0]+"/json"
-			c = requests.get(new_url)
-			print  c.status_code
+        final_data = []
+        for j in words:
+            val = random.randint(0, 4)
+            print api_keys[val]
+            new_url = "http://words.bighugelabs.com/api/2/" + \
+                str(api_keys[val]) + "/" + j[0] + "/json"
+            c = requests.get(new_url)
+            print c.status_code
 
-			if c.status_code == 200:
+            if c.status_code == 200:
 
-				result = c.json()
-				# print result
-				# print result['noun']['syn']
+                result = c.json()
+                # print result
+                # print result['noun']['syn']
 
-				try:
-					result =result['noun']['syn']
-					# print result+"-----------"
-					result = result+j
-					print result
-				except:
-					result = j[0]
-					print list(result)
-			for k in result:
-				# print k
-				for i in keys:
-					# print i
-					if k.lower() in i.lower():
-						# print i
-						# print k
-						final_data.append(data[i])
-			print final_data
-		if len(final_data) == 0:
-			print "sry didn't understand "
+                try:
+                    result = result['noun']['syn']
+                    # print result+"-----------"
+                    result = result + j
+                    print result
+                except:
+                    result = j[0]
+                    print list(result)
+            for k in result:
+                # print k
+                for i in keys:
+                    # print i
+                    if k.lower() in i.lower():
+                        # print i
+                        # print k
+                        final_data.append(data[i])
+            print final_data
+        if len(final_data) == 0:
+            print "sry didn't understand "
 
 
 def noun(answer):
-	# if "everything" in answer.lower():
-	#     pass
-	stop_words = set(stopwords.words('english'))
-	text = nltk.word_tokenize(answer)
+    # if "everything" in answer.lower():
+    #     pass
+    stop_words = set(stopwords.words('english'))
+    text = nltk.word_tokenize(answer)
 
-	filtered_sentence = [w for w in text if not w in stop_words]
-	print filtered_sentence
-	# stemmed = snowball_stemmer.stem(answer)
-	# print stemmed
+    filtered_sentence = [w for w in text if not w in stop_words]
+    print filtered_sentence
+    # stemmed = snowball_stemmer.stem(answer)
+    # print stemmed
 
-	tags =  nltk.pos_tag(filtered_sentence)
+    tags = nltk.pos_tag(filtered_sentence)
 
-	words =  [[word] for word, tag in tags if tag in ('NN','NNP')]
-	print words
-	read_json(words)
+    words = [[word] for word, tag in tags if tag in ('NN', 'NNP')]
+    print words
+    read_json(words)
 
-    # if len(words)==1:
-    #     text_to_speech.get_speech("Are you looking for"+ words[0])
-    #     answer = speech_to_text.stt()
-    #     print answer
-    #     if "yes" in answer.lower() or "ye" in answer.lower():
-    #         pass
-    # elif(len(words)>1):
-    #     a = ""
-    #     a = " or ".join(words)
-    #     print a
-    #
-    #     text_to_speech.get_speech("What are you looking for"+ a)
-    #     answer = speech_to_text.stt()
-    #     if answer in words:
-    #         pass
-
-
+# if len(words)==1:
+#     text_to_speech.get_speech("Are you looking for"+ words[0])
+#     answer = speech_to_text.stt()
+#     print answer
+#     if "yes" in answer.lower() or "ye" in answer.lower():
+#         pass
+# elif(len(words)>1):
+#     a = ""
+#     a = " or ".join(words)
+#     print a
+#
+#     text_to_speech.get_speech("What are you looking for"+ a)
+#     answer = speech_to_text.stt()
+#     if answer in words:
+#         pass
 
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    # app.run(debug=True)
+    # read_json()
+    # nltk.help.upenn_tagset('NN')
+    # nltk.help.upenn_tagset('NNP')
+    # nltk.help.upenn_tagset('NNS')
 
-
-if __name__ == '__main__' :
-	# port = int(os.environ.get('PORT', 8080))
-	# app.run(host='0.0.0.0',port=port)
-	# app.run(debug=True)
-	# read_json()
-	# nltk.help.upenn_tagset('NN')
-	# nltk.help.upenn_tagset('NNP')
-	# nltk.help.upenn_tagset('NNS')
-
-	# noun("Hey allo so what do you have for my falling hair.")
-	# noun("Hey allo my skin  is hair getting dry")
-	# noun("Which soap are good to use")
-	# noun("Hey allo How to keep my teeth shiny")
-	# noun("i have skin sweating problems")
-	noun("ok so ariel i am having some Facial problems")
+    # noun("Hey allo so what do you have for my falling hair.")
+    # noun("Hey allo my skin  is hair getting dry")
+    # noun("Which soap are good to use")
+    # noun("Hey allo How to keep my teeth shiny")
+    # noun("i have skin sweating problems")
+    # noun("ok so ariel i am having some Facial problems")
